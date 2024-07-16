b4c547ec8a97c4709073811d7162a63a1a7ab55d
AdityaPradhan7 <aditya7pradhan@gmail.com>
2024-07-16 18:54:05 +0530

    first commit

---
+++ src/Components/Card.js
@@ -0,0 +1,42 @@
+import react from "react";
+import { useState } from "react/cjs/react.development";
+import Modal from "./Modal";
+const Card = ({ book }) => { 
+    // 'book' is a prop and an array as we got from Main.js
+
+    const [show,setShow]=useState(false);
+    const [bookItem,setItem]=useState();
+    console.log(book)
+    return (
+        <>
+            {
+                book.map((item) => {
+                    //  item is a variable that represents an individual book object within the book array prop.
+                    // When you use the .map() function to iterate over the book array, item is a reference to the
+                    // current element in the array during each iteration.
+                    let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+                    let amount=item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+                    if(thumbnail!= undefined && amount !=undefined)
+                    {
+                        return (
+                            <>
+                            <div className="card" onClick={()=>{setShow(true);setItem(item)}}>
+                                <img src={thumbnail} alt="" />
+                                <div className="bottom">
+                                    <h3 className="title">{item.volumeInfo.title}</h3>
+                                    <p className="amount">&#8377;{amount}</p>
+                                    {/* &#8377 is rupee symbol code for HTML */}
+                                </div>
+                            </div>
+                              <Modal show={show} item={bookItem} onClose={()=>setShow(false)}/>
+                            </>
+                        )
+                    }
+                    
+                })
+            }
+
+        </>
+    )
+}
+export default Card;
---
+++ src/Components/Main.js
@@ -0,0 +1,60 @@
+import react ,{useState}from "react";
+import Card from "./Card";
+import SearchIcon from "./search.svg";
+import axios from "axios";
+
+const Main=()=>{
+    const [search,setSearch]=useState("");
+    const [bookData,setData]=useState([]);
+    const searchBook=(evt)=>{
+        if(evt.key==="Enter")
+        {
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU&maxResults=40')
+            // here seacrh is our search query, for eg if we seacrh java, then it will show results reltaed to java
+            .then((res)=>setData(res.data.items)) // res = response
+            .catch((err)=>console.log(err)) // err = error
+        }
+    }
+
+    const handleClick=()=>{
+
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU&maxResults=40')
+            // here seacrh is our search query, for eg if we seacrh java, then it will show results reltaed to java
+            .then((res)=>setData(res.data.items)) // res = response
+            .catch((err)=>console.log(err)) // err = error
+    }
+
+    return(
+        <>
+            <div className="header">
+                <div className="row1">
+                    <h1>A room without books is like<br/> a body without a soul.</h1>
+                </div>
+                <div className="row2">
+                    <h2>Find Your Book</h2>
+                    <div className="search">
+                        <input type="text" placeholder="Enter Your Book Name"
+                        value={search} onChange={e=>setSearch(e.target.value)}
+                        onKeyPress={searchBook}/>
+                        <img
+                        src={SearchIcon}
+                        alt="search"
+                        onClick={handleClick}
+                        />
+                    </div>
+                    <img src="./images/bg2.png" alt="kids reading" />
+                    <h3>NOTE - Results show only saleable books</h3>
+                </div>
+            </div>
+
+            <div className="container">
+
+              {
+                    <Card book={bookData}/>
+                    // the bookData state, which is an array, is passed to the Card component as a prop
+              }  
+            </div>
+        </>
+    )
+}
+export default Main;
